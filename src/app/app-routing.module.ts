1324df842cb4692cd412e61f0fcf0d516695f12b
Facundo Caamaño <facundoadriancaamano@outlook.com>
2025-02-08 16:26:16 -0300

    rutas, inicio, nosotros

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './dashboard/pages/inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'inicio',
+    component: InicioComponent
+  },
+  {
+    path:'**',
+    redirectTo:'inicio'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
