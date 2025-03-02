1c606d6bf1a5fddc283a99fb6cce56d9a2a91c84
Facundo Caamaño <facundoadriancaamano@outlook.com>
2025-03-02 17:44:02 -0300

    pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './dashboard/pages/inicio/inicio.component';
+import { CervezasComponent } from './dashboard/pages/cervezas/cervezas.component';
+import { NosotrosComponent } from './dashboard/pages/nosotros/nosotros.component';
+import { ContactoComponent } from './dashboard/pages/contacto/contacto.component';
 
 const routes: Routes = [
   {
@@ -8,8 +11,16 @@ const routes: Routes = [
     component: InicioComponent
   },
   {
-    path:'**',
-    redirectTo:'inicio'
+    path:'cervezas',
+    component:CervezasComponent
+  },
+  {
+    path:'nosotros',
+    component:NosotrosComponent
+  },
+  {
+    path:'contacto',
+    component:ContactoComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,27 @@ import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { NavbarComponent } from './dashboard/components/navbar/navbar.component';
 import { FooterComponent } from './dashboard/components/footer/footer.component';
+import { CervezasComponent } from './dashboard/pages/cervezas/cervezas.component';
+import { ContactoComponent } from './dashboard/pages/contacto/contacto.component';
+import { NosotrosComponent } from './dashboard/pages/nosotros/nosotros.component';
+import { RouterModule } from '@angular/router';
+import { InicioComponent } from './dashboard/pages/inicio/inicio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
+    InicioComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    CervezasComponent,
+    ContactoComponent,
+    NosotrosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
